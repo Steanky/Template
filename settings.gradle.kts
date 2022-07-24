0229de669bfb5a51e027ab7f8b9791b34858f9c8
steank <dnd.initiative.tracker@gmail.com>
2022-07-23 18:20:10 -0700

    better README.md

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,10 +2,11 @@ rootProject.name = "template"
 
 val localSettings = file("local.settings.gradle.kts")
 if (localSettings.exists()) {
+    // Local settings can be used for environment variables, secrets, or other things that shouldn't be on VCS
     apply(localSettings)
 }
 
-
+// New modules are added by appending them to the sequenceOf function
 sequenceOf("java").forEach {
     val projectDirectory = file(it)
     include(":${rootProject.name}-$it")
